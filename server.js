5d057efd406681349df202fff8d227781f91f2c6
Abdullah Waqet <a.waqet@morniksa.com>
2019-04-19 22:20:11 +0300

    refactor: simplify code by utilize async and await

--- server.js
+++ server.js
@@ -13,38 +13,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // add end-point to get block   GET /block/{block-height}
-app.get("/block/:blockHeight", function(req, res) {
-  let height = req.params.blockHeight;
-  let block = chain
-    .getBlock(height)
-    .then(block => {
-      if (block) {
-        res.send(block);
-      } else {
-        res.status(404).send({ error: `block Height ${height} not exists` });
-      }
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500).send({ error: "Something went wrong" });
-    });
+app.get("/block/:blockHeight", async function(req, res) {
+  let block = await chain.getBlock(req.params.blockHeight);
+  try {
+    if (!block) {
+      res.status(404).send({ error: `block Height ${height} not exists` });
+      return;
+    }
+    res.send(block);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: "Something went wrong" });
+  }
 });
 // add end-point to add block   POST /block
-app.post("/block", function(req, res) {
-  console.log(req.body.body);
-  if (req.body.body) {
-    let block = chain
-      .addBlock(req.body.body)
-      .then(block => {
-        console.log(block);
-        res.send(block);
-      })
-      .catch(error => {
-        console.log(error);
-        res.status(500).send({ error: "Something went wrong" });
-      });
-  } else {
+app.post("/block", async function(req, res) {
+  let body = req.body.body;
+
+  if (!body) {
     res.status(400).send({ error: `Invalid block data` });
+  }
+
+  try {
+    let block = await chain.addBlock(body);
+    res.send(block);
+  } catch (error) {
+    res.status(500).send({ error: "Something went wrong" });
   }
 });
 // start express app

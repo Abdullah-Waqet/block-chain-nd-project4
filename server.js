281a7ca837fc12404b3e2e6a87003aca81aacede
Abdullah Waqet <a.waqet@morniksa.com>
2019-04-19 22:12:08 +0300

    feat: update end-points to handle async process

--- server.js
+++ server.js
@@ -15,18 +15,34 @@ app.use(bodyParser.json());
 // add end-point to get block   GET /block/{block-height}
 app.get("/block/:blockHeight", function(req, res) {
   let height = req.params.blockHeight;
-  let block = chain.getBlock(req.params.height);
-  if (block) {
-    res.send(block);
-  } else {
-    res.status(404).send({ error: `block Height ${height} not exists` });
-  }
+  let block = chain
+    .getBlock(height)
+    .then(block => {
+      if (block) {
+        res.send(block);
+      } else {
+        res.status(404).send({ error: `block Height ${height} not exists` });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send({ error: "Something went wrong" });
+    });
 });
 // add end-point to add block   POST /block
 app.post("/block", function(req, res) {
+  console.log(req.body.body);
   if (req.body.body) {
-    let block = chain.addBlock(req.body.body);
-    res.send(block);
+    let block = chain
+      .addBlock(req.body.body)
+      .then(block => {
+        console.log(block);
+        res.send(block);
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).send({ error: "Something went wrong" });
+      });
   } else {
     res.status(400).send({ error: `Invalid block data` });
   }

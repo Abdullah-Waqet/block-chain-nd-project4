41a9b01d14c8ba70e2b653904b1aae8f65a22906
Abdullah Waqet <a.waqet@morniksa.com>
2019-04-19 21:20:21 +0300

    refactor: rename key's to meet rubrics requirment

--- block.js
+++ block.js
@@ -1,11 +1,14 @@
 class Block {
-    constructor(data) {
-        this.height = 0;
-        this.time = new Date().getTime().toString().slice(0, -3);
-        this.hash = '';
-        this.previousHash = '';
-        this.data = data;
-    }
+  constructor(data) {
+    this.height = 0;
+    this.time = new Date()
+      .getTime()
+      .toString()
+      .slice(0, -3);
+    this.hash = "";
+    this.previousBlockHash = "";
+    this.body = data;
+  }
 }
 
 module.exports = Block;
--- blockChain.js
+++ blockChain.js
@@ -1,35 +1,33 @@
-const crypto = require('crypto-js/sha256');
-const Block = require('./block');
+const crypto = require("crypto-js/sha256");
+const Block = require("./block");
 
 class BlockChain {
+  constructor() {
+    this.chain = [];
+    this.addBlock("First block in the chain - Genesis block");
+  }
 
-    constructor() {
-        this.chain = [];
-        this.addBlock('Genesis Block');
+  getBlockHeight() {
+    return this.chain.length;
+  }
+
+  addBlock(data) {
+    let block = new Block(data);
+    block.height = this.getBlockHeight();
+    block.hash = crypto(JSON.stringify(block)).toString();
+
+    if (block.height > 0) {
+      block.previousBlockHash = this.chain[block.height - 1].hash;
     }
 
-    getBlockHeight() {
-        return this.chain.length;
-    }
+    this.chain.push(block);
 
-    addBlock(data) {
-        let block = new Block(data);
-        block.height = this.getBlockHeight();
-        block.hash = crypto(JSON.stringify(block)).toString();
+    return block;
+  }
 
-        if (block.height > 0) {
-            block.previousHash = this.chain[block.height - 1].hash;
-        }
-
-        this.chain.push(block);
-
-        return block;
-    }
-
-    getBlock(height) {
-        return this.chain[height];
-    }
-
+  getBlock(height) {
+    return this.chain[height];
+  }
 }
 
 module.exports = () => new BlockChain();

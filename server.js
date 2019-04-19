ad439d8bcbc42f1684516d3bfd542b7abfa5a0da
Abdullah Waqet <a.waqet@morniksa.com>
2019-04-19 21:22:34 +0300

    feat: add get/post end points - add get block end-point - add post block end point -  config body parser

--- server.js
+++ server.js
@@ -6,11 +6,30 @@ const chain = require("./blockChain")();
 const app = express();
 
 // use body parser to to enable JSON body to be presented in request object
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 // add end-point to get block   GET /block/{block-height}
-
-
+app.get("/block/:blockHeight", function(req, res) {
+  let height = req.params.blockHeight;
+  let block = chain.getBlock(req.params.height);
+  if (block) {
+    res.send(block);
+  } else {
+    res.status(404).send({ error: `block Height ${height} not exists` });
+  }
+});
 // add end-point to add block   POST /block
-
+app.post("/block", function(req, res) {
+  if (req.body.body) {
+    let block = chain.addBlock(req.body.body);
+    res.send(block);
+  } else {
+    res.status(400).send({ error: `Invalid block data` });
+  }
+});
 // start express app
 app.listen(8000, () => console.log(`Example app listening on port ${8000}!`));

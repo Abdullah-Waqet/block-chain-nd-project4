e55b9112183b01560ad10e3c9dff08a16c16227c
Abdullah Waqet <a.waqet@morniksa.com>
2019-04-19 21:21:06 +0300

    feat: init server

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const myBlockChain = require('./blockChain')();
-
-const port = 8000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const chain = require("./blockChain")();
 
 // initialize express app
-const app = ;
+const app = express();
 
 // use body parser to to enable JSON body to be presented in request object
 
@@ -16,3 +13,4 @@ const app = ;
 // add end-point to add block   POST /block
 
 // start express app
+app.listen(8000, () => console.log(`Example app listening on port ${8000}!`));

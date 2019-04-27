cd5a2d6ddc80093dfbea9c5d29919baf1a4e8973
Abdullah Waqet <a.waqet@morniksa.com>
2019-04-27 20:06:44 +0300

    fix: validation rules

--- bodyValidators.js
+++ bodyValidators.js
@@ -13,16 +13,18 @@ module.exports = {
   },
   messageValidator(body) {
     const schema = {
-      address: Joi.string()
+      address: joi
+        .string()
         .min(10)
         .max(34)
         .required(),
-      signature: Joi.string()
+      signature: joi
+        .string()
         .max(88)
         .required()
     };
 
-    return joi.validate(req.body, schema);
+    return joi.validate(body, schema);
   },
   blockValidator(body) {
     const schema = {
@@ -31,24 +33,27 @@ module.exports = {
         .min(10)
         .max(34)
         .required(),
-      star: joi.object().keys({
-        dec: joi
-          .string()
-          .min(3)
-          .required(),
-        ra: joi
-          .string()
-          .min(3)
-          .required(),
-        magnitude: joi.string().min(3),
-        constellation: joi.string().min(3),
-        story: joi
-          .string()
-          .min(3)
-          .regex(/[a-zA-Z0-9]/)
-          .required()
-      })
+      star: joi
+        .object()
+        .keys({
+          dec: joi
+            .string()
+            .min(3)
+            .required(),
+          ra: joi
+            .string()
+            .min(3)
+            .required(),
+          magnitude: joi.string().min(3),
+          constellation: joi.string().min(3),
+          story: joi
+            .string()
+            .min(3)
+            .regex(/[a-zA-Z0-9]/)
+            .required()
+        })
+        .required()
     };
-    return joi.validate(req.body, schema);
+    return joi.validate(body, schema);
   }
 };
